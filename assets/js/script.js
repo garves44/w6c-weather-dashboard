aaa8a22fe09ae231d70a646425c2dc2e603309a4
Garves <garves44@gmail.com>
2020-06-03 17:09:08 -0600

    updated forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,13 @@
  * 0. Globals
  * 1. Functions
  *   1.1 getWeather()
- *   1.2 citySearch()
- *   1.3 displayWeather()
+ *   1.2 getForecast()
+ *   1.3 citySearch()
+ *   1.4 displayWeather()
+ *   1.5 displayForecast()
  * 
  * 2. Document Ready
- *   2.1 Render Schedule on ready
+ *   2.1 Render Weather on ready
  *   2.2 Add click listeners (add, edit, delete, reset)
  * 
  * 3. Dead Code
@@ -21,7 +23,9 @@
  *********************************************************/
 
 /* ===============[ 0. GLOBALS ]=========================*/
-
+var cityEl = document.getElementById("city");
+var displayForecastEl = document.getElementById("display-forecast");
+var displayWeatherEl = document.getElementById("display");
 /* ===============[ 1. Functions ]=========================*/
 
 /**
@@ -29,13 +33,15 @@
  */
 var getWeather = function (city) {
 
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=904755abfca69992b8a848481a87baea";
+
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=904755abfca69992b8a848481a87baea";
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayWeather(data, city);
+                    console.log("response ok");
+                    displayWeather(data);
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -47,14 +53,39 @@ var getWeather = function (city) {
 };
 
 /**
- * 1.2 citySearch()
+ * 1.2 getForecast()
+ */
+var getForecast = function (city) {
+
+
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=904755abfca69992b8a848481a87baea";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log("response ok");
+                     displayForecast(data);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to gather weather data!");
+        });
+};
+
+/**
+ * 1.3 citySearch()
  */
 var citySearch = function (event) {
     event.preventDefault();
-    var city = $('#city').value.trim();
+    var city = cityEl.value.trim();
 
     if (city) {
         getWeather(city);
+        getForecast(city);
         city.value = "";
     } else {
         alert("Please enter a city name!");
@@ -63,16 +94,101 @@ var citySearch = function (event) {
 };
 
 /**
- * 1.3 displayWeather()
+ * 1.4 displayWeather()
  */
-var displayWeather = function (weather, city) {
+
+var displayWeather = function (data) {
+    var currentDate = moment.unix(data.dt).format("MM/DD/YYYY");
+
+    var weatherEl = document.createElement("div");
+    weatherEl.classList = "list-item flex-row justify-space-between align-center";
+
+    var cityDateEl = document.createElement("h2");
+    cityDateEl.classList = "list-item flex-row justify-space-between col-sm"
+    cityDateEl.textContent = `${data.name} ${currentDate} ${data.weather[0].icon}`
+    weatherEl.appendChild(cityDateEl);
+
+    var tempEl = document.createElement("div");
+    tempEl.classList = "list-item flex-row justify-space-between col-sm"
+    tempEl.textContent = `Temp: ${data.main.temp}`;
+    weatherEl.appendChild(tempEl);
+
+    var humidityEl = document.createElement("div");
+    humidityEl.classList = "list-item flex-row justify-space-between col-sm"
+    humidityEl.textContent = `Humidity: ${data.main.humidity}`;
+    weatherEl.appendChild(humidityEl);
+
+    var windSpeedEl = document.createElement("div");
+    windSpeedEl.classList = "list-item flex-row justify-space-between col-sm"
+    windSpeedEl.textContent = `Wind Speed: ${data.wind.speed}`;
+    weatherEl.appendChild(windSpeedEl);
+
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.classList = "list-item flex-row justify-space-between col-sm"
+    uvIndexEl.textContent = `UV Index INCOMPLETED`;
+    weatherEl.appendChild(uvIndexEl);
+
+    displayWeatherEl.appendChild(weatherEl);
+};
+
+/**
+ * 1.5 displayForecast()
+ */
+var displayForecast = function (data) {
     
+
+    // var dateObj = new Date(data.list[0].dt * 1000);
+    // var month = dateObj.getUTCMonth().toString();
+    // var day = dateObj.getUTCDay().toString();
+    // var year = dateObj.getUTCFullYear().toString();
+    // var currentDate = `(${month}/${day}/${year})`;
+    // console.log("currentDate", currentDate);
+    console.log("data = ", data);
+    console.log("data.list = ", data.list);
+    console.log("data.list[0] = ", data.list[0]);
+    console.log("data.list[5] = ", data.list[5]);
+    
+
+    for (var i = 0; i < data.list.length; i = i + 8) {
+        var currentDate = moment.unix(data.list[i].dt).format("MM/DD/YYYY");
+        console.log("for loop has started and is on position", i);
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var dateEl = document.createElement("div");
+        dateEl.classList = "list-item flex-row justify-space-between col-sm"
+        dateEl.textContent = currentDate;
+        forecastEl.appendChild(dateEl);
+
+        // var iconEl =  NEEDS TO BE DONE STILL
+
+        var tempEl = document.createElement("div");
+        tempEl.classList = "list-item flex-row justify-space-between col-sm"
+        tempEl.textContent = `Temp: ${data.list[i].main.temp}`;
+        forecastEl.appendChild(tempEl);
+
+        var humidityEl = document.createElement("div");
+        humidityEl.classList = "list-item flex-row justify-space-between col-sm"
+        humidityEl.textContent = `Humidity: ${data.list[i].main.humidity}`;
+        forecastEl.appendChild(humidityEl);
+
+        displayForecastEl.appendChild(forecastEl);
+    }
 };
 
 
 
 /* ===============[ 2. Document Ready ]=========================*/
 
+/**
+ * 2.1 Render Weather on ready
+ */
+
+/**
+ * 2.2 Add click listeners (add, edit, delete, reset)
+ */
+
+$("#submit-weather").on("click", citySearch);
 
 
 
